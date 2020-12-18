81eb3502c85676cee8ab0eab338b5a0e24d86225
ash.midgley <ashleymidgley@protonmail.com>
2020-12-19 11:26:03 +1300

    Add test for post routes.

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -33,6 +35,44 @@ func TestGetRoutes(t *testing.T) {
 			response, err := http.Get(fmt.Sprintf("%s/api/%s", server.URL, tc.route))
 			if err != nil {
 				t.Fatalf("could not send GET request: %v", err)
+			}
+			defer response.Body.Close()
+
+			if response.StatusCode != tc.status {
+				t.Errorf("expected status %v; got %v", tc.status, response.Status)
+			}
+
+			_, err = ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Fatalf("could not read response: %v", err)
+			}
+		})
+	}
+}
+
+func TestPostRoutes(t *testing.T) {
+	tt := []struct {
+		name   string
+		route  string
+		body   string
+		status int
+	}{
+		{name: "create world leaderboard entry", route: "world/leaderboard", body: "", status: http.StatusUnauthorized},
+	}
+
+	server := httptest.NewServer(router())
+	defer server.Close()
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			jsonString, err := json.Marshal(tc.body)
+			if err != nil {
+				t.Error("failed to parse test body")
+			}
+
+			response, err := http.Post(fmt.Sprintf("%s/api/%s", server.URL, tc.route), "application/json", bytes.NewReader(jsonString))
+			if err != nil {
+				t.Fatalf("could not send POST request: %v", err)
 			}
 			defer response.Body.Close()
 
