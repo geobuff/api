4373b4574f4c8e4db324c70517fad37eee571e96
Ash Midgley <ashleymidgley@protonmail.com>
2021-04-26 09:02:55 +1200

    Fixxy fix.

--- config/config_test.go
+++ config/config_test.go
@@ -4,27 +4,27 @@ import "testing"
 
 func TestLoad(t *testing.T) {
 	tt := []struct {
-		name     string
-		filename string
-		err      bool
+		name        string
+		filename    string
+		shouldError bool
 	}{
 		{
-			name:     "valid filename",
-			filename: "../config.json",
-			err:      false,
+			name:        "valid filename",
+			filename:    "../config.json",
+			shouldError: false,
 		},
 		{
-			name:     "invalid filename",
-			filename: "test.json",
-			err:      true,
+			name:        "invalid filename",
+			filename:    "test.json",
+			shouldError: true,
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			err := Load(tc.filename)
-			if err != nil && tc.err {
-				t.Fatal("expected error; got none")
+			if err != nil && !tc.shouldError {
+				t.Fatalf("unexpected error %v", err)
 			}
 		})
 	}
