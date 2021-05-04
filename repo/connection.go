459513c5f30d4d479d1d7734d48783463dde9a03
Ash Midgley <ashleymidgley@protonmail.com>
2021-05-05 10:29:28 +1200

    Shrink db vars to single connection string.

--- repo/connection.go
+++ repo/connection.go
@@ -2,7 +2,6 @@ package repo
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 )
 
@@ -11,15 +10,11 @@ var Connection *sql.DB
 
 // OpenConnection initializes the Connection variable.
 var OpenConnection = func() error {
-	connection, err := sql.Open("postgres", getConnectionString())
+	connection, err := sql.Open("postgres", os.Getenv("CONNECTION_STRING"))
 	if err != nil {
 		return err
 	}
 
 	Connection = connection
 	return Connection.Ping()
-}
-
-func getConnectionString() string {
-	return fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"))
 }
