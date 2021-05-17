b1e95b72f34d4294aa3c8eba2d26350fc35b36f0
Ash Midgley <ashleymidgley@protonmail.com>
2021-05-18 11:49:21 +1200

    Add filter on deployment branch.

--- env.sh
+++ env.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-echo SITE_URL=$SITE_URL >> .env
-echo CONNECTION_STRING=$CONNECTION_STRING >> .env
-echo AUTH_SIGNING_KEY=$AUTH_SIGNING_KEY >> .env
-echo AUTH_ISSUER=$AUTH_ISSUER >> .env
-echo CORS_ORIGINS=$CORS_ORIGINS >> .env
+if [ "$CIRCLE_BRANCH" = "prod-pipeline" ]; then
+    echo SITE_URL=$DEV_SITE_URL >> .env
+    echo CONNECTION_STRING=$DEV_CONNECTION_STRING >> .env
+    echo AUTH_ISSUER=$DEV_AUTH_ISSUER >> .env
+    echo AUTH_SIGNING_KEY=$DEV_AUTH_SIGNING_KEY >> .env
+    echo CORS_ORIGINS=$DEV_CORS_ORIGINS >> .env
+elif [ "$CIRCLE_BRANCH" = "main" ]; then
+    echo SITE_URL=$PROD_SITE_URL >> .env
+    echo CONNECTION_STRING=$PROD_CONNECTION_STRING >> .env
+    echo AUTH_ISSUER=$PROD_AUTH_ISSUER >> .env
+    echo AUTH_SIGNING_KEY=$PROD_AUTH_SIGNING_KEY >> .env
+    echo CORS_ORIGINS=$PROD_CORS_ORIGINS >> .env
+fi
+
 echo CORS_METHODS=$CORS_METHODS >> .env
 echo CORS_HEADERS=$CORS_HEADERS >> .env
 echo SENDGRID_API_KEY=$SENDGRID_API_KEY >> .env
