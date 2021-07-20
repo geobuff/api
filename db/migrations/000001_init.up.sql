5be09cfaddf04523ff2c537d8ae20866f1737552
Ash Midgley <ashleymidgley@protonmail.com>
2021-07-20 19:28:50 +1200

    Oops add missing plays.

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -187,7 +187,8 @@ INSERT INTO plays (quizId, value) values
 (30, 0),
 (31, 0),
 (32, 0),
-(33, 0);
+(33, 0),
+(34, 0);
 
 INSERT INTO merch (name, description, price, disabled) values
 ('GeoTee', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 69.99, FALSE),
