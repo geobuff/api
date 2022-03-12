e344b82ed936402cc521eb12c3c1403e5bcecc86
Ash Midgley <ashleymidgley@gmail.com>
2022-03-12 16:22:25 +1300

    Add get user community quizzes endpoint.

--- repo/communityquizzes.go
+++ repo/communityquizzes.go
@@ -68,6 +68,25 @@ func GetFirstCommunityQuizID(offset int) (int, error) {
 	return id, err
 }
 
+func GetUserCommunityQuizzes(userID int) ([]CommunityQuizDto, error) {
+	statement := "SELECT q.id, q.userid, u.username, q.name, q.description, q.maxscore, q.added FROM communityquizzes q JOIN users u ON u.id = q.userid WHERE q.userId = $1;"
+	rows, err := Connection.Query(statement, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var quizzes = []CommunityQuizDto{}
+	for rows.Next() {
+		var quiz CommunityQuizDto
+		if err = rows.Scan(&quiz.ID, &quiz.UserID, &quiz.Username, &quiz.Name, &quiz.Description, &quiz.MaxScore, &quiz.Added); err != nil {
+			return nil, err
+		}
+		quizzes = append(quizzes, quiz)
+	}
+	return quizzes, rows.Err()
+}
+
 func InsertCommunityQuiz(quiz CreateCommunityQuizDto) error {
 	statement := "INSERT INTO communityquizzes (userid, name, description, maxscore, added) VALUES ($1, $2, $3, $4, $5) RETURNING id;"
 	var quizID int
