c42020dc2407d03d785b1bb568f932619a731142
Ash Midgley <ashleymidgley@gmail.com>
2022-06-26 17:12:54 +1200

    Add extra quizzes.

--- db/migrations/000023_alter_trivia_plays_table.down.sql
+++ db/migrations/000023_alter_trivia_plays_table.down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE triviaplays ALTER COLUMN triviaid SET NOT NULL;
+/* 
+NOTE: Removed below command as not possible to reintroduce contraint after trivia that matches triviaId has been removed from trivia table.  
 ALTER TABLE triviaplays ADD CONSTRAINT triviaplays_triviaid_fkey FOREIGN KEY (triviaid) REFERENCES trivia(id);
+*/
