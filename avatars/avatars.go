589d9feda04c02b4af23b3c87a6a111db38af392
Ash Midgley <ashleymidgley@protonmail.com>
2022-11-24 16:34:31 +1300

    Comment out translate code

--- avatars/avatars.go
+++ avatars/avatars.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/geobuff/api/helpers"
 	"github.com/geobuff/api/repo"
 )
 
@@ -16,39 +15,39 @@ func GetAvatars(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	language := request.Header.Get("Content-Language")
-	if language != "" && language != "en" {
-		translatedAvatars := make([]repo.AvatarDto, len(avatars))
-		for index, avatar := range avatars {
-			avatarType, err := helpers.TranslateText(language, avatar.Type)
-			if err != nil {
-				http.Error(writer, fmt.Sprintf("%v\n", err), http.StatusInternalServerError)
-				return
-			}
+	// language := request.Header.Get("Content-Language")
+	// if language != "" && language != "en" {
+	// 	translatedAvatars := make([]repo.AvatarDto, len(avatars))
+	// 	for index, avatar := range avatars {
+	// 		avatarType, err := helpers.TranslateText(language, avatar.Type)
+	// 		if err != nil {
+	// 			http.Error(writer, fmt.Sprintf("%v\n", err), http.StatusInternalServerError)
+	// 			return
+	// 		}
 
-			description, err := helpers.TranslateText(language, avatar.Description)
-			if err != nil {
-				http.Error(writer, fmt.Sprintf("%v\n", err), http.StatusInternalServerError)
-				return
-			}
+	// 		description, err := helpers.TranslateText(language, avatar.Description)
+	// 		if err != nil {
+	// 			http.Error(writer, fmt.Sprintf("%v\n", err), http.StatusInternalServerError)
+	// 			return
+	// 		}
 
-			translatedAvatars[index] = repo.AvatarDto{
-				ID:                avatar.ID,
-				Type:              avatarType,
-				CountryCode:       avatar.CountryCode,
-				FlagUrl:           avatar.FlagUrl,
-				Name:              avatar.Name,
-				Description:       description,
-				PrimaryImageUrl:   avatar.PrimaryImageUrl,
-				SecondaryImageUrl: avatar.SecondaryImageUrl,
-				GridPlacement:     avatar.GridPlacement,
-			}
-		}
+	// 		translatedAvatars[index] = repo.AvatarDto{
+	// 			ID:                avatar.ID,
+	// 			Type:              avatarType,
+	// 			CountryCode:       avatar.CountryCode,
+	// 			FlagUrl:           avatar.FlagUrl,
+	// 			Name:              avatar.Name,
+	// 			Description:       description,
+	// 			PrimaryImageUrl:   avatar.PrimaryImageUrl,
+	// 			SecondaryImageUrl: avatar.SecondaryImageUrl,
+	// 			GridPlacement:     avatar.GridPlacement,
+	// 		}
+	// 	}
 
-		writer.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(translatedAvatars)
-		return
-	}
+	// 	writer.Header().Set("Content-Type", "application/json")
+	// 	json.NewEncoder(writer).Encode(translatedAvatars)
+	// 	return
+	// }
 
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(avatars)
