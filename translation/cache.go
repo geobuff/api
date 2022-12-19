adf9ccb7dd6596d4cd1c2c543174ac4054099333
Ash Midgley <ashleymidgley@protonmail.com>
2022-12-19 19:05:27 +1300

    Replace NoExpiration with DefaultExpiration in translation cache.

--- translation/cache.go
+++ translation/cache.go
@@ -5,5 +5,5 @@ import "github.com/patrickmn/go-cache"
 var c *cache.Cache
 
 func InitCache() {
-	c = cache.New(cache.NoExpiration, cache.NoExpiration)
+	c = cache.New(cache.DefaultExpiration, cache.DefaultExpiration)
 }
